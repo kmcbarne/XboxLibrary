5eddf39d707d42c4fc83b74e133c582ecb61ad1f
Keven McBarnes <Keven.McBarnes@gmail.com>
2024-01-08 16:39:51 -0600

    Commited MultiPlatformSetup for the first time

---
+++ XboxLibrary/Console.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace XboxLibrary
+{
+    public class Console
+    {
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Name.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NameProperty =
+            DependencyProperty.Register("Name", typeof(string), typeof(Console), new PropertyMetadata(null));
+
+
+    }
+}
